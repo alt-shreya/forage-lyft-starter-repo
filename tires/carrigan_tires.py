a3e6c44a1315d4af57377da1227b41fbfcfb5373
alt-shreya <shreyakumar31@gmail.com>
2024-01-18 18:50:15 +0530

    code: updated feature logic

--- tires/carrigan_tires.py
+++ tires/carrigan_tires.py
@@ -4,4 +4,4 @@ class carrigan_tires(Tires):
     def __init__(self, wear_sensor_readings:list) -> None:
         self.wear_sensor_readings = wear_sensor_readings
     def needs_service(self):
-        return 
+        return any(num >= 0.9 for num in self.wear_sensor_readings)
